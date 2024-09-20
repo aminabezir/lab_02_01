edb6341dbd20ea06220fb00b1af93afdefca18e7
Погонин Нил Сергеевич <m2400164@edu.misis.ru>
2024-09-20 15:58:31 +0300

    Добавлена проверка введенного числа

---
+++ check_input.py
@@ -0,0 +1,11 @@
+def check_input(number, string):
+    if string == "color":
+        if number in range(1, 7):
+            return True
+        else:
+            return False
+    elif string == "detail":
+        if number in range(1, 7):
+            return True
+        else:
+            return False
